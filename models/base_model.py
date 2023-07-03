fe24447be1ee96c980077a52116261c7b232a6db
User <user@Users-MacBook.local>
2023-07-03 13:44:16 +0200

    try

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,21 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''init func'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    setattr(self, key, type(self))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         cl_name = self.__class__.__name__
