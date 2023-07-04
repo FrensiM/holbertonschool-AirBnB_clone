b7cc02a29b17b4923e3239811da9b4a22a66b664
User <user@Users-MacBook.local>
2023-07-04 10:23:21 +0200

    try

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,11 +31,9 @@ class FileStorage:
 
     def reload(self):
         '''reload fun'''
-        try:
-            with open(FileStorage.__file_path,
-                      encoding="utf-8", mode="r") as f:
-                for key, value in json.load(f).items():
-                    value = eval(value["__class__"].items())
-                    self.__objects[key] = value
-        except FileNotFoundError:
-            pass
+        if FileStorage.__file_path:
+                with open(FileStorage.__file_path,
+                       encoding="utf-8", mode="r") as f:
+                    for key, value in json.load(f).items():
+                        value = eval(value["__class__"].items())
+                        self.__objects[key] = value
